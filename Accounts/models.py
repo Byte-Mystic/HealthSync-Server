e8fcf9b4c19ebb64a9b342e84280d9ae4d81ae01
Rishi060Mishra <110656861+Rishi060Mishra@users.noreply.github.com>
2024-02-28 12:31:25 +0530

    Initial Commit

---
+++ Accounts/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(max_length=100, unique=True)
+    phone = models.IntegerField(blank=True, null=True)
+    role = models.CharField(max_length=50, default="user")
+    last_seen = models.DateTimeField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    def __str__(self):
+        return self.username
+
