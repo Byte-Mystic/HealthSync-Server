de587d5797511e6143df24f7c9c7f9e3ce6207aa
CS-Aditya-Rawat <adityainferno4812@gmail.com>
2024-04-27 19:27:16 +0530

    Updated Chat Function

--- AiChat/utils.py
+++ AiChat/utils.py
@@ -1,12 +1,15 @@
-from dotenv import load_dotenv
+from openai import OpenAI
 
-# import google.generativeai as palm
-
-load_dotenv()
-# palm.configure(api_key="AIzaSyDyyBhtKdy5Zys8RjN5cs--wF6RRvxqvoA")
+client = OpenAI(api_key="API KEY")
 
 
 def chat_with_me(question: str) -> str | None:
-    return "Answer"
-    # response = palm.chat(messages=[question])
-    # return response.last
+    # try:
+    #     chat_completion = client.chat.completions.create(
+    #         messages=[{"role": "user", "content": question}],
+    #         model="gpt-3.5-turbo",
+    #     )
+    #     print(chat_completion)
+    # except Exception as e:
+    #     print(f"error in generating Answer: {e}")
+    return "The Asnwer will be AI generated."
